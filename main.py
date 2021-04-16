52168adf1a4b418203b70a33f646ca0c69c01211
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-16 22:34:22 +0200

    refactored dataloader

--- main.py
+++ main.py
@@ -12,72 +12,9 @@ import sys
 from model import ImageClassifier
 from config import get_args
 import utils
+import dataloaders
+import model
 
-
-# Dataloaders
-def get_dataloaders(args):
-    if args.dataset == 'mnist':
-        transform = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(), lambda x: x.repeat(3, 1, 1)])
-        ds_train = MNIST('./data', train=True, download=True, transform=transform)
-        ds_validation = MNIST('./data', train=False, download=True, transform=transform)
-        dl_train = DataLoader(ds_train, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-        dl_validation = DataLoader(ds_validation, batch_size=args.batch_size, shuffle=False, pin_memory=True)
-        n_cl, c, h, w = 10, 3, 32, 32
-
-    elif args.dataset == 'cifar10':
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        transform_validation = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-        ds_train = CIFAR10('./data', train=True, download=True, transform=transform_train)
-        ds_validation = CIFAR10('./data', train=False, download=True, transform=transform_validation)
-        dl_train = DataLoader(ds_train, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-        dl_validation = DataLoader(ds_validation, batch_size=args.batch_size, shuffle=False, pin_memory=True)
-        n_cl, c, h, w = 10, 3, 32, 32
-
-    else:
-        raise Exception(f'Error. Dataset "{args.dataset}" is not supported.')
-
-    data_info = {
-        'num_classes': n_cl,
-        'height': h,
-        'width': w,
-        'channels': c,
-    }
-    dls_dict = {
-        'train': dl_train,
-        'validation': dl_validation,
-    }
-    return dls_dict, data_info
-
-
-# Model
-def get_model(args, data_info):
-    model_args = {
-        'num_classes': data_info['num_classes'], 
-        'quantized': args.quantized, 
-        'num_emb': args.num_embeddings, 
-        'in_dim': 512,
-        'beta': args.beta,
-        'lr': args.lr,
-        'dropout': args.dropout,
-        'vq_mode': args.vq_mode,
-        'decay': args.decay,
-        'beta': args.beta,
-        'temp_init': args.temp_init,
-        'straight_through': args.straight_through,
-    }
-    model = ImageClassifier(**model_args)
-    if len(args.model_checkpoint) > 0:
-        model = model.load_from_checkpoint(args.model_checkpoint, **model_args)
-        print('Loaded checkpoints at "{args.model_checkpoint}"')
-    return model
 
 def get_trainer(args):
     trainer_args = {
@@ -94,10 +31,10 @@ def get_trainer(args):
 def main(args):
 
     # Dataloader
-    dls, data_info = get_dataloaders(args)
+    dls, data_info = dataloaders.get_dataloaders(args)
 
     # Model
-    model = get_model(args, data_info)
+    model = model.get_model(args, data_info)
 
     # Trainer
     trainer = get_trainer(args)
