c688713d0df6ae67617094bca03d20307e69fd47
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-16 19:48:05 +0200

    added utils

---
+++ utils.py
@@ -0,0 +1,29 @@
+##################################################
+# Imports
+##################################################
+
+import pytorch_lightning as pl
+
+def get_callbacks(args):
+
+    # Callbacks
+    model_ckpt = pl.callbacks.model_checkpoint.ModelCheckpoint(
+        dirpath=None,
+        filename='best',
+        monitor='validation_acc',
+        verbose=True,
+        save_last=True,
+        mode='max',    
+    )
+    model_ckpt.CHECKPOINT_NAME_LAST = '{epoch}-{step}'
+    callbacks = [model_ckpt]
+    return callbacks
+
+    def get_logger(args):
+
+        # Logger
+        logger = pl.loggers.tensorboard.TensorBoardLogger(
+            save_dir='./tmp',
+            name=f'{args.dataset}',
+        )
+    return logger
