4753de2f3eb8d07f42ae4141724ebd456f4e768f
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-16 20:39:10 +0200

    improved args

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def get_args(stdin):
     parser.add_argument('--dataset', type=str, help='The dataset.')
     parser.add_argument('--batch_size', type=int, default=256, help='The batch size.')
     parser.add_argument('--lr', type=float, default=3e-4, help='The learning rate.')
-    parser.add_argument('--epochs', type=int, default=30, help='The number of epochs for the learning process.')
+    parser.add_argument('--epochs', type=int, default=100, help='The number of epochs for the learning process.')
     parser.add_argument('--quantized', action='store_true', help='Use quantized network.')
     parser.add_argument('--beta', type=float, default=0.25, help='Loss weight (see VQVAE paper).')
     parser.add_argument('--decay', type=float, default=0.99, help='Decay factor in the EMA embeddings learning (see VQVAE paper).')
